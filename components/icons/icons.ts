75acba16a87255ceed9d8024eab684ae6cf72b07
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-04-18 14:02:03 -0300

    feat(icons): add additional SVG icons for WhatsApp, Mail, Phone, and Download

--- components/icons/icons.ts
+++ components/icons/icons.ts
@@ -1,11 +1,18 @@
 import Linkedin from '@/public/icons/linkedin.svg'
 import Menu from '@/public/icons/menu.svg'
 import Location from '@/public/icons/location.svg'
-
+import WhatsApp from '@/public/icons/whatsapp.svg'
+import Mail from '@/public/icons/mail.svg'
+import Phone from '@/public/icons/phone.svg'
+import Download from '@/public/icons/download.svg'
 
 export const icons = {
   Linkedin,
   Menu,
   Location,
+  WhatsApp,
+  Mail,
+  Phone,
+  Download
 }
 
