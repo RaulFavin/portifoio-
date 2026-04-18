f2fd56c9007a4c75957a421f85c8eea2d0364b1b
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-04-18 14:22:31 -0300

    feat(Home): add metadata for SEO optimization

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,10 @@ import { EducationSection } from "./components/EducationSection";
 import { Header } from "./components/Header";
 import { LanguagesSection } from "./components/LanguagesSection";
 
+export const metadata = {
+  title: 'Raul Favin | Mixologist Portfolio',
+  description: 'Discover the art of mixology through the portfolio of Raul Favin, a passionate and innovative mixologist. Explore his signature creations, seasonal cocktails, and unique approach to crafting unforgettable drinking experiences.',
+}
 export default function Home() {
   return (
     <main>
