29e73972300d9aa63bbe1ad879ddd3556f170b60
Censuradho <gustavoleiteoliveira800@gmail.com>
2026-04-18 13:02:52 -0300

    feat(icons): add Location icon to the icons collection

--- components/icons/icons.ts
+++ components/icons/icons.ts
@@ -1,9 +1,11 @@
 import Linkedin from '@/public/icons/linkedin.svg'
 import Menu from '@/public/icons/menu.svg'
+import Location from '@/public/icons/location.svg'
 
 
 export const icons = {
   Linkedin,
   Menu,
+  Location,
 }
 
